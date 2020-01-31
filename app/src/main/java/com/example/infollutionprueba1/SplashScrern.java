ebd79e516aff4935be33fcdba24cf83dd6aa6252
carlosFDDV <carlosfernandezdiezdiez@gmail.com>
2020-01-31 13:02:38 +0100

    oso en splash con animación

--- app/src/main/java/com/example/infollutionprueba1/SplashScrern.java
+++ app/src/main/java/com/example/infollutionprueba1/SplashScrern.java
@@ -2,25 +2,44 @@ package com.example.infollutionprueba1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class SplashScrern extends AppCompatActivity {
+public class SplashScrern extends Activity {
 
-    TextView tvMovimiento;
+    ImageView ivFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_scrern);
 
-        View tvMovimiento = findViewById(R.id.tvMovimiento);
+        View ivFoto = findViewById(R.id.ivLogo_SS);
 
-        Animation translate = AnimationUtils.loadAnimation(this, R.anim.grow_disappear);
+        Animation translate = AnimationUtils.loadAnimation(this, R.anim.leftin);
 
-        tvMovimiento.startAnimation(translate);
+        ivFoto.startAnimation(translate);
+
+        openApp(true);
+    }
+
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScrern
+                        .this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
