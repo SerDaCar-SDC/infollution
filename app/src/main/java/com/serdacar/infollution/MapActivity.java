3dfe77311e21b722ad8809a141b3a66121b11372
porteroFitness <sergy05.gm@gmail.com>
2020-02-07 14:02:53 +0100

    Añadido Clase MapActivity Añadido implementaciones de google location services Añadido dependencias de google play service en Gradle Scripts

--- app/src/main/java/com/serdacar/infollution/MapActivity.java
+++ app/src/main/java/com/serdacar/infollution/MapActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -23,6 +24,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_map);
 
         // METER GOOGLE MAPS
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentMap);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -33,11 +37,28 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         // LatLng sidney = new LatLng(-34, 151);
 
-        LatLng alco = new LatLng(39.506417, 2.751009);
+        // COORDENADAS
+        LatLng llMadrid = new LatLng(40.4165001, -3.7025599);
+        LatLng llNorte = new LatLng(40.6590900, -3.7676200);
+        LatLng llNoroeste = new LatLng(40.6350600, -4.0048600);
+        LatLng llSuroeste = new LatLng(40.4500600, -3.9834400);
+        LatLng llSur = new LatLng(40.3223381 , -3.86496);
+        LatLng llSureste = new LatLng(40.3007600, -3.4372200);
+        LatLng llNordeste = new LatLng(40.4820500, -3.3599600);
 
-        mMap.addMarker(new MarkerOptions().position(alco).title("Marker in MALLORCAA"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(alco, 13));
+        // MARCADORES EN COORDENADAS
+        mMap.addMarker(new MarkerOptions().position(llMadrid).title("Marcador en Madrid capital"));
+        mMap.addMarker(new MarkerOptions().position(llNorte).title("Marcador en Colmenar Viejo"));
+        mMap.addMarker(new MarkerOptions().position(llNoroeste).title("Marcador en Collado Villalba"));
+        mMap.addMarker(new MarkerOptions().position(llSuroeste).title("Marcador en Villanueva de la Cañada"));
+        mMap.addMarker(new MarkerOptions().position(llSur).title("Marcador en Móstoles"));
+        mMap.addMarker(new MarkerOptions().position(llSureste).title("Marcador en Arganda del Rey"));
+        mMap.addMarker(new MarkerOptions().position(llNordeste).title("Marcador en Alcalá de Henares"));
 
+        // POSICIÓN DE CÁMARA
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(llMadrid, 10));
+
+        // TIPO DE VISUALIZACIÓN DE MAPA
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
