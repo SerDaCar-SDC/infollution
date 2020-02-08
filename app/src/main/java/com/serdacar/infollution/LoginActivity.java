982cbf97068134afb4d393fed999f8c8c1bc2942
porteroFitness <sergy05.gm@gmail.com>
2020-02-08 18:02:06 +0100

    Funciona Google Maps

--- app/src/main/java/com/serdacar/infollution/LoginActivity.java
+++ app/src/main/java/com/serdacar/infollution/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void accesoFirstActivity(View view) {
-        Intent i = new Intent(this, MapsActivity.class);
+        Intent i = new Intent(this, MapActivity.class);
         startActivity(i);
     }
 }
