7415d014ee0f5180b36feb01b6f784dba933084c
porteroFitness <sergy05.gm@gmail.com>
2020-03-16 14:57:16 +0100

    Commit ubicacion

--- app/src/main/java/com/serdacar/infollution/MapActivity.java
+++ app/src/main/java/com/serdacar/infollution/MapActivity.java
@@ -118,7 +118,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         //-------Métodos para consultar la baes de datos--------//
         persistencia = new EstacionDataSource(this);
 
-        /* * * * * * * * * * * * * ** * * * * * **/
+        /* * * * * * * * * * * * * * * * * * * * * */
         // TODO: leerEstacion();
     }
 
@@ -134,6 +134,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        // LOCALIZACION EN TIEMPO REAL
+        mMap.setMyLocationEnabled(true);
         LatLng ubicacion = new LatLng(miLoc.getLatitude(), miLoc.getAltitude());
 
         /*if (miLoc == null) {
@@ -216,32 +219,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
          */
 
 
-        /*
-        listaEstaciones.add(new Estacion(4, "Pza. de España", "Plaza de España", 40.4238823, -3.7122567));
-        listaEstaciones.add(new Estacion(8, "Escuelas Aguirre", "Entre C/ Alcalá y C/ O’ Donell", 40.4215533, -3.6823158));
-        listaEstaciones.add(new Estacion(11, "Avda. Ramón y Cajal", "Avda. Ramón y Cajal  esq. C/ Príncipe de Vergara", 40.4514734, -3.6773491));
-        listaEstaciones.add(new Estacion(16, "Arturo Soria", "C/ Arturo Soria  esq. C/  Vizconde de los Asilos", 40.4400457, -3.6392422));
-        listaEstaciones.add(new Estacion(17, "Villaverde", "C/. Juan Peñalver", 40.347147, -3.7133167));
-        listaEstaciones.add(new Estacion(18, "Farolillo", "Calle Farolillo - C/Ervigio", 40.3947825, -3.7318356));
-        listaEstaciones.add(new Estacion(24, "Casa de Campo", "Casa de Campo  (Terminal del Teleférico)", 40.4193577, -3.7473445));
-        listaEstaciones.add(new Estacion(27, "Barajas Pueblo", "C/. Júpiter, 21 (Barajas)", 40.4769179, -3.5800258));
-        listaEstaciones.add(new Estacion(35, "Pza. del Carmen", "Plaza del Carmen esq. Tres Cruces", 40.4192091, -3.7031662));
-        listaEstaciones.add(new Estacion(36, "Moratalaz", "Avd. Moratalaz  esq. Camino de los Vinateros", 40.4079517, -3.6453104));
-        listaEstaciones.add(new Estacion(38, "Cuatro Caminos", "Avda. Pablo Iglesias esq. C/ Marqués de Lema", 40.4455439, -3.7071303));
-        listaEstaciones.add(new Estacion(39, "Barrio del Pilar", "Avd. Betanzos esq. C/  Monforte de Lemos", 40.4782322, -3.7115364));
-        listaEstaciones.add(new Estacion(40, "Vallecas", "C/ Arroyo del Olivar  esq. C/  Río Grande", 40.3881478, -3.6515286));
-        listaEstaciones.add(new Estacion(47, "Mendez Alvaro", "C/ Juan de Mariana / Pza. Amanecer Méndez Alvaro", 40.3980991, -3.6868138));
-        listaEstaciones.add(new Estacion(48, "Castellana", "C/ José Gutiérrez Abascal", 40.4398904, -3.6903729));
-        listaEstaciones.add(new Estacion(49, "Parque del Retiro", "Paseo Venezuela- Casa de Vacas", 40.4144444, -3.6824999));
-        listaEstaciones.add(new Estacion(50, "Plaza Castilla", "Plaza Castilla (Canal)", 40.4655841, -3.6887449));
-        listaEstaciones.add(new Estacion(54, "Ensanche de Vallecas", "Avda La Gavia / Avda. Las Suertes", 40.3730118, -3.6121394));
-        listaEstaciones.add(new Estacion(55, "Urb. Embajada", "C/ Riaño (Barajas) ", 40.4623628, -3.5805649));
-        listaEstaciones.add(new Estacion(56, "Pza. Elíptica", " Pza. Elíptica - Avda. Oporto", 40.3850336, -3.7187679));
-        listaEstaciones.add(new Estacion(57, "Sanchinarro", "C/ Princesa de Éboli esq C/ María Tudor", 40.4942012, -3.6605173));
-        listaEstaciones.add(new Estacion(58, "El Pardo", "Avda. La Guardia", 40.4942012, -3.6605173));
-        listaEstaciones.add(new Estacion(59, "Juan Carlos I", "Parque Juan Carlos I (frente oficinas mantenimiento)", 40.4607255, -3.6163407));
-        listaEstaciones.add(new Estacion(60, "Tres Olivos", "Plaza Tres Olivos", 40.5005477, -3.6897308));
-         */
 
         // POSICIÓN DE CÁMARA
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uem, 20));
