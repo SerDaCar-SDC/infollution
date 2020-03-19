6f278d670733674c684031896f2030125adbfba1
carlosFDDV <carlosfernandezdiezdiez@gmail.com>
2020-03-19 16:57:02 +0100

    CArlos Dani unidas, falta First

--- app/src/main/java/com/serdacar/infollution/FirstActivity.java
+++ app/src/main/java/com/serdacar/infollution/FirstActivity.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.alespero.expandablecardview.ExpandableCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.serdacar.infollution.database.EstacionDataSource;
