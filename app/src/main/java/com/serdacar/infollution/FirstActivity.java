fc62e5f49fa4c53203d8f0698dc7331111e68b61
danielSBPC <daniel.sanchezbo@gmail.com>
2020-03-20 14:54:57 +0100

    Gráfica con consulta estación mas cercana

--- app/src/main/java/com/serdacar/infollution/FirstActivity.java
+++ app/src/main/java/com/serdacar/infollution/FirstActivity.java
@@ -63,12 +63,6 @@ public class FirstActivity extends AppCompatActivity {
     ImageView ivLogo;
     String email;
 
-    // GRAFICAS
-    String so2;
-    String co;
-    String no;
-    String no2;
-
     private LocationManager miLocalizacion;
     private Location loc;
 
@@ -96,11 +90,9 @@ public class FirstActivity extends AppCompatActivity {
         /* * * *  ENCONTRAR UBICACION * * * * * * * * * * * * * * * * * */
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            Toast.makeText(this, "Es necesario que active su GPS", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Es necesario que active la localización GPS", Toast.LENGTH_LONG).show();
             return;
         } else {
-
             miLocalizacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             loc = miLocalizacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         }
@@ -109,16 +101,16 @@ public class FirstActivity extends AppCompatActivity {
         miLatitud = loc.getLatitude();
         miLongitud = loc.getLongitude();
 
-        Toast.makeText(this, miLatitud + " " + miLongitud, Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, miLatitud + " " + miLongitud, Toast.LENGTH_LONG).show();
 
-        EstacionDataSource eds = new EstacionDataSource(this);
+        EstacionDataSource persistencia = new EstacionDataSource(this);
 
-        ArrayList<Estacion> listaEstaciones = eds.leerEstacionLista();
+        ArrayList<Estacion> listaEstaciones = persistencia.leerEstacionLista();
         final Estacion estacionCerca = encontrarEstMasCerca(listaEstaciones, miLatitud, miLongitud);
 
         estacionCerca.getLatitud();
         estacionCerca.getLongitud();
-        Toast.makeText(this, estacionCerca.getNombre(), Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, estacionCerca.getNombre(), Toast.LENGTH_LONG).show();
 
         /* * * * * * * * * * * * * * * * * * * * * */
 
@@ -126,10 +118,15 @@ public class FirstActivity extends AppCompatActivity {
         APIEstaciones apiEstaciones = retrofit.create(APIEstaciones.class);
         Call<Datos> call  = apiEstaciones.obtenerDatos();
 
-        /*call.enqueue(new Callback<Datos>() {
+        call.enqueue(new Callback<Datos>() {
             @Override
             public void onResponse(Call<Datos> call, Response<Datos> response) {
                 if(response.isSuccessful()) {
+                    String so2 = "";
+                    String co = "";
+                    String no = "";
+                    String no2 = "";
+
                     Datos d = response.body();
                     List<DatoHorario> listaEstaciones = d.getDatoHorario();
 
@@ -139,12 +136,12 @@ public class FirstActivity extends AppCompatActivity {
 
                     int horaActual = comprobarHora();
 
+                    //Estacion encontrada = persistencia.leerEstacion(estacionCerca.getCodigoCorto());
+
                     for(int i = 0; i < listaEstaciones.size(); i++) {
                         codigoEstacion = listaEstaciones.get(i).getEstacion();
 
                         if (codigoEstacion.substring(0, 1).equals("0")
-
-                                //estacionSeleccionada
                                 && estacionCerca.getCodigoCorto() == Integer.parseInt(listaEstaciones.get(i).getEstacion())) {
                             codigoRecuperado = codigoEstacion.substring(1, 3);
                             numeroEstacion = Integer.parseInt(codigoRecuperado);
@@ -369,7 +366,6 @@ public class FirstActivity extends AppCompatActivity {
                                 }
                             }
                         } else if (codigoEstacion.substring(0,2).equals("00")
-                                // estacionSeleccionada
                                 && estacionCerca.getCodigoCorto() == Integer.parseInt(listaEstaciones.get(i).getEstacion())){
                             codigoRecuperado = codigoEstacion.substring(2, 3);
                             numeroEstacion = Integer.parseInt(codigoRecuperado);
@@ -595,10 +591,40 @@ public class FirstActivity extends AppCompatActivity {
                             }
                         }
                     }
+                    double dioxidoAzufre = Double.parseDouble(so2);
+                    double monoxidoNitrogeno = Double.parseDouble(co);
+                    double monoxidoCarbono = Double.parseDouble(no);
+                    double dioxidoNitrogeno = Double.parseDouble(no2);
+
+                    AnyChartView anyChartView = findViewById(R.id.acGrafica);
+
+                    Pie pie = AnyChart.pie();
+
+                    List<DataEntry> data = new ArrayList<>();
+                    data.add(new ValueDataEntry("Dióxido de Azufre (SO2)", dioxidoAzufre));
+                    data.add(new ValueDataEntry("Monóxido de Nitrógeno (CO)", monoxidoNitrogeno));
+                    data.add(new ValueDataEntry("Monóxido de Carbono (NO)", monoxidoCarbono));
+                    data.add(new ValueDataEntry("Dióxido de Nitrógeno (NO2)", dioxidoNitrogeno));
+
+                    pie.data(data);
+
+                    pie.animation(true);
+
+                    pie.title("Estación más cercana: " + estacionCerca.getNombre()).padding(20d, 0d, 0d, 0d);
+
+                    pie.labels().position("outside");
+
+                    pie.legend().title().text("").padding(70d, 50d, 0d, 50d);
+                    pie.legend().position("center-bottom").itemsLayout(LegendLayout.HORIZONTAL).align(Align.CENTER);
+
+                    anyChartView.setChart(pie);
+
                 } else {
                     Log.e("ERROR ON RESPONSE", "ERROR: " + response.code());
                 }
             }
+
+
 
             public int comprobarHora() {
                 Date hora;
@@ -614,117 +640,9 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
                 Log.e("ERROR ON FAILURE", "ERROR: " + t.getMessage());
             }
-        });*/
+        });
 
-        // TODO: Toast.makeText(this, "DA: " + da + ", LE: " + le + ", MC: " + mc + ", DN: " + dn, Toast.LENGTH_LONG).show();
-
-
-       /* * * * * * * * * * * * * * * * ** * * * * * * * * * ** * * * * * * * * * **
-
-        AnyChartView anyChartView = findViewById(R.id.acGrafica);
-
-        CircularGauge circularGauge = AnyChart.circular();
-        circularGauge.data(new SingleValueDataSet(new String[] { "7", "7", "7", "7", "100", "100"}));
-        circularGauge.fill("#fff").stroke(null).padding(0d, 0d, 0d, 0d).margin(100d, 100d, 100d, 100d);
-        circularGauge.startAngle(0d); // 0d
-        circularGauge.sweepAngle(270d); // 270d
-
-        Circular xAxis = circularGauge.axis(0).radius(100d).width(1d).fill((Fill) null);
-        xAxis.scale().minimum(0d).maximum(100d);
-        xAxis.ticks("{ interval: 5 }").minorTicks("{ interval: 5 }");
-        xAxis.labels().enabled(false);
-        xAxis.ticks().enabled(true);
-        xAxis.minorTicks().enabled(true);
-
-
-
-
-
-        circularGauge.label(0d).text("Dioxido de Azufre").useHtml(true).hAlign(String.valueOf(HAlign.CENTER)).vAlign(String.valueOf(HAlign.CENTER));
-        circularGauge.label(0d).anchor(Anchor.RIGHT_CENTER).padding(0d, 10d, 0d, 0d).height(17d / 2d + "%").offsetY(100d + "%").offsetX(0d);
-
-        Bar bar0 = circularGauge.bar(0d);
-        bar0.dataIndex(0d);
-        bar0.radius(100d);
-        bar0.width(17d);
-        bar0.fill(new SolidFill("#64b5f6", 1d));
-        bar0.stroke(null);
-        bar0.zIndex(5d);
-        Bar bar100 = circularGauge.bar(100d);
-        bar100.dataIndex(5d);
-        bar100.radius(100d);
-        bar100.width(17d);
-        bar100.fill(new SolidFill("#F5F4F4", 1d));
-        bar100.stroke("1 #e5e4e4");
-        bar100.zIndex(4d);
-
-        circularGauge.label(1d).text("Monóxido de Carbono").useHtml(true).hAlign(String.valueOf(HAlign.CENTER)).vAlign(String.valueOf(HAlign.CENTER));
-        circularGauge.label(1d).anchor(Anchor.RIGHT_CENTER).padding(0d, 10d, 0d, 0d).height(17d / 2d + "%").offsetY(80d + "%").offsetX(0d);
-        Bar bar1 = circularGauge.bar(1d);
-        bar1.dataIndex(1d);
-        bar1.radius(80d);
-        bar1.width(17d);
-        bar1.fill(new SolidFill("#1976d2", 1d));
-        bar1.stroke(null);
-        bar1.zIndex(5d);
-        Bar bar101 = circularGauge.bar(101d);
-        bar101.dataIndex(5d);
-        bar101.radius(80d);
-        bar101.width(17d);
-        bar101.fill(new SolidFill("#F5F4F4", 1d));
-        bar101.stroke("1 #e5e4e4");
-        bar101.zIndex(4d);
-
-        circularGauge.label(2d).text("Dioxido de Nitrogeno").useHtml(true).hAlign(String.valueOf(HAlign.CENTER)).vAlign(String.valueOf(HAlign.CENTER));
-        circularGauge.label(2d).anchor(Anchor.RIGHT_CENTER).padding(0d, 10d, 0d, 0d).height(17d / 2d + "%").offsetY(60d + "%").offsetX(0d);
-        Bar bar2 = circularGauge.bar(2d);
-        bar2.dataIndex(2d);
-        bar2.radius(60d);
-        bar2.width(17d);
-        bar2.fill(new SolidFill("#ef6c00", 1d));
-        bar2.stroke(null);
-        bar2.zIndex(5d);
-        Bar bar102 = circularGauge.bar(102d);
-        bar102.dataIndex(5d);
-        bar102.radius(60d);
-        bar102.width(17d);
-        bar102.fill(new SolidFill("#F5F4F4", 1d));
-        bar102.stroke("1 #e5e4e4");
-        bar102.zIndex(4d);
-
-        circularGauge.label(3d).text("Dióxido de Carbono").useHtml(true).hAlign(String.valueOf(HAlign.CENTER)).vAlign(String.valueOf(HAlign.CENTER));
-        circularGauge.label(3d).anchor(Anchor.RIGHT_CENTER).padding(0d, 10d, 0d, 0d).height(17d / 2d + "%").offsetY(40d + "%").offsetX(0d);
-        Bar bar3 = circularGauge.bar(3d);
-        bar3.dataIndex(3d);
-        bar3.radius(40d);
-        bar3.width(17d);
-        bar3.fill(new SolidFill("#ffd54f", 1d));
-        bar3.stroke(null);
-        bar3.zIndex(5d);
-        Bar bar103 = circularGauge.bar(103d);
-        bar103.dataIndex(5d);
-        bar103.radius(40d);
-        bar103.width(17d);
-        bar103.fill(new SolidFill("#F5F4F4", 1d));
-        bar103.stroke("1 #e5e4e4");
-        bar103.zIndex(4d);
-
-
-        circularGauge.margin(50d, 50d, 50d, 50d);
-        circularGauge.title().text("MEDIDOR DE CONTAMINACIÓN' +\n" + "    '<br/><span style=\"color:#929292; font-size: 12px;\">(ESTACIONES DE LA COMUNIDAD DE MADRID").useHtml(true);
-        circularGauge.title().enabled(true);
-        circularGauge.title().hAlign(String.valueOf(HAlign.CENTER));
-        circularGauge.title().padding(0d, 0d, 0d, 0d).margin(0d, 0d, 20d, 0d);
-
-        anyChartView.setChart(circularGauge);
-
-        * * * * * * * * * * * * * * * ** * * * * * * * * * ** * * * * * * * * * **/
-
-
-
-
-       // EJEMPLO OFICIAL DE LA PAGINA
-        AnyChartView anyChartView = findViewById(R.id.acGrafica);
+        /*AnyChartView anyChartView = findViewById(R.id.acGrafica);
 
         Pie pie = AnyChart.pie();
 
@@ -734,28 +652,23 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this, event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
             }
         });
-        // TODO: Parsear a double
-        /*int dioxidoAzufre = Integer.parseInt(so2);
-        int monoxidoNitrogeno = Integer.parseInt(co);
-        int monoxidoCarbono = Integer.parseInt(no);
-        int dioxidoNitrogeno = Integer.parseInt(no2);*/
 
         List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("Dioxido de Azufre (SO2)", 0.2));
-        data.add(new ValueDataEntry("Monoxido de Nitrogeno (CO)", 16.2));
-        data.add(new ValueDataEntry("Monoxido de Carbono (NO)", 10.0));
-        data.add(new ValueDataEntry("Dioxido de Nitrogeno (NO2", 23.2));
+        data.add(new ValueDataEntry("Dioxido de Azufre (SO2)", dioxidoAzufre));
+        data.add(new ValueDataEntry("Monoxido de Nitrogeno (CO)", monoxidoNitrogeno));
+        data.add(new ValueDataEntry("Monoxido de Carbono (NO)", monoxidoCarbono));
+        data.add(new ValueDataEntry("Dioxido de Nitrogeno (NO2", dioxidoNitrogeno));*/
 
-        pie.data(data);
+        //pie.data(data);
 
         // pie.palette("#DE3655");
         // pie.hatchFill(Html.);
         // pie.palette(String.valueOf(getColor(R.color.colorAzulOscuro)));
         // pie.hatchFillPalette(getColor(R.color.colorAzulOscuro), getColor(R.color.colorVerde))
 
-        pie.animation(true);
+       /* pie.animation(true);
 
-        pie.title("Estacion más cerca " + estacionCerca.getNombre())/*.padding(50d, 50d, 0d, 50d)*/;
+        pie.title("Estacion más cerca " + estacionCerca.getNombre()).padding(50d, 50d, 0d, 50d);
 
         pie.labels().position("outside");
 
@@ -763,85 +676,10 @@ public class FirstActivity extends AppCompatActivity {
         pie.legend().title().text("Leyenda").padding(50d, 0d, 0d, 50d);
         pie.legend().position("center-bottom").itemsLayout(LegendLayout.HORIZONTAL).align(Align.CENTER);
 
-        anyChartView.setChart(pie);
-
-
-
-
-
-
-
-
-
-
-        /*
-        pie1 = AnyChart.pie();
-        // pie2 = CircularGauge.instantiate()
-
-        List<DataEntry> data = new ArrayList<>();
-
-        data.add(new ValueDataEntry("Dioxido de Azufre", Integer.parseInt(da)));
-        data.add(new ValueDataEntry("Monoxido de Carbono", Integer.parseInt(mc)));
-        data.add(new ValueDataEntry("Dioxido de Nitrogeno", Integer.parseInt(le)));
-        data.add(new ValueDataEntry("Dioxido de Nitrogeno", Integer.parseInt(dn)));
-
-        pie1.data(data);
-        graficaTarta = (AnyChartView) findViewById(R.id.acGrafica);
-        graficaTarta.setChart(pie1);
-        */
-
-
-/*        Retrofit retrofit = RetrofitClient.getClient(APIEstaciones.BASE_URL);
-        APIEstaciones apiEstaciones = retrofit.create(APIEstaciones.class);
-        Call<Datos> call  = apiEstaciones.obtenerDatos();
-
-        String da;
-        String mc;
-        String no;
-
-        call.enqueue(new Callback<Datos>() {
-            @Override
-            public void onResponse(Call<Datos> call, Response<Datos> response) {
-                if(response.isSuccessful()) {
-                    Datos d = response.body();
-                    List<DatoHorario> listaEstaciones = d.getDatoHorario();
-                    List<DataEntry> data = new ArrayList<>();
-
-                    for(int i = 0; i < listaEstaciones.size(); i++) {
-
-                        listaEstaciones.get(i).getEstacion();
-                        listaEstaciones.get(i).getH02();
-                        listaEstaciones.get(i).getH03();
-                    }
-
-                    pie1.data(data);
-                    graficaTarta.setChart(pie1);
-
-
-                } else {
-                    Log.e("ERROR ON RESPONSE", "ERROR: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Datos> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-                Log.e("ERROR ON FAILURE", "ERROR: " + t.getMessage());
-            }
-        });*/
-
-        /*graficaTarta = (AnyChartView) findViewById(R.id.acGrafica);
-        graficaTarta.setChart(pie1);*/
-
-
+        anyChartView.setChart(pie);*/
 
     }
 
-
-    /* * * * * * * * * * * * * * * * * * * * * * * * * * ** * */
-
-    // lat = x \\
-    // long = y   \\
     public Estacion encontrarEstMasCerca(ArrayList<Estacion> lista, double posiX, double posiY) {
         Estacion resMenosdistante = null;
 
